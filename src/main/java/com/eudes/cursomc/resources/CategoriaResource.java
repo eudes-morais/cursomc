3605dff7f147e4a8f3c415b145f7237b99f216c0
Eudes Morais <eudes.morais@gmail.com>
2019-10-01 00:48:00 -0300

    REST funcionando

---
+++ src/main/java/com/eudes/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,13 @@
+package com.eudes.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoriaResource {
+	
+	@GetMapping(value = "/categorias")
+	public String listar() {
+		return "REST funcionado!";
+	}
+}
